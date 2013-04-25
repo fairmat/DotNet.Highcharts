d2edd02225a40f03785847bda0edab0a47426009
Stefano Angeleri <weltall2@gmail.com>
2013-04-25 19:59:05 +0200

    Added an additional sample for Highstock.
    The additional sample is in Chart Types/Area Spline Range.

--- DotNet.Highcharts/DotNet.Highcharts.Samples/Controllers/DemoController.cs
+++ DotNet.Highcharts/DotNet.Highcharts.Samples/Controllers/DemoController.cs
@@ -2161,6 +2161,35 @@ namespace DotNet.Highcharts.Samples.Controllers
             return View(chart);
         }
 
+        public ActionResult HighstockAreaSplineRange()
+        {
+            Highstock chart = new Highstock("chart")
+                .InitChart(new Chart
+                {
+                    DefaultSeriesType = ChartTypes.Areasplinerange,
+                    ClassName = "chart"
+                })
+                // range selector seems to have problems in highstock.
+                /*.SetRangeSelector(new RangeSelector
+                {
+                    Selected = 2,
+                })*/
+                .SetTitle(new Title
+                {
+                    Text = "Temperature variation by day",
+                })
+                .SetTooltip(new Tooltip
+                {
+                    ValueSuffix = "°C"
+                })
+                .SetSeries(new[]
+                {
+                    new Series { Name = "Temperatures" },
+                });
+
+            return View(chart);
+        }
+
         public ActionResult HighstockScrollbarDisabled()
         {
             Highstock chart = new Highstock("chart")
